52cb806995dfb3dcba30f4b4d6a1ab56e022025c
Nicoly <nicolyrrh@gmail.com>
2025-10-15 18:55:07 -0300

    feat: adicionado novos exercícios em C

--- Logica-de-programacao/C/Atividades/Lista 4/exe59t.c
+++ Logica-de-programacao/C/Atividades/Lista 4/exe59t.c
@@ -4,8 +4,9 @@
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
-    float salario = 0, salariom = 0, salarioh = 0;
+    float salario = 0, salariom = 0,media=0, salarioh = 0,h=0,f=0, idmj=0;
     char sexo, resp;
+    int idade;
     //char sexo2;
     
     int idade = 0, idadem = 0;
@@ -20,18 +21,28 @@ int main()
         {
             idadem = idade;
         }
-        printf("Digite 'F' para Feminino e 'M' para Masculino: ");
+       printf("Digite 'F' para Feminino e 'M' para Masculino: ");
         scanf(" %c", &sexo);
       //  sexo2=toupper(sexo);
-        if(sexo=='M' && sexo=='m')
+        if(sexo=='M' && sexo=='m'){
+            h++;
+            media=media+idade;
+        }
+         if(sexo=='F' && sexo=='f'){
+            idmj=idade;
+    
+        }
         printf("\nDeseja continuar? ");
         scanf(" %c", &resp);
 
     } while (resp != 'n' && resp != 'N');
 
     printf("-----------------------------------");
-    printf("\nSALÁRIO TOTAL MASCULINO: ");
-    printf("\nSALÁRIO TOTAL FEMININO:");
+    printf("\nMAIOR IDADE LIDA: %f \n",media/h);
+    printf("\nMAIOR IDADE LIDA: %f \n",media/h);
+     printf("\nMAIOR IDADE LIDA: %f \n",media/h);
+          printf("\nMAIOR IDADE LIDA: %f \n",media/h);
+    printf("\n:");
 
     return 0;
 }
